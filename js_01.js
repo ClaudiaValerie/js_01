ec9181c5a4b013af0a8dda654189c8049afbc73a
Claudia Valeria <claudiamendezalvarado@gmail.com>
2023-11-06 12:12:52 +0100

    index.html

---
+++ js_01.js
@@ -0,0 +1,19 @@
+// VARIABLES 
+
+numero1 = 10 //variable de tipo entero
+
+console.log(typeof numero1)
+numero2 = 102.3 //variable de tipo decimal
+console.log(typeof numero2)
+
+texto1 = "Esto es un texto"
+console.log(typeof texto1)
+
+verdadero = true
+console.log(typeof verdadero)
+
+<Let></Let> verdadero = true
+console.log(typeof verdadero)
+
+//document.getElementById("js1").innerText = numero1
+console.log(numero1)
